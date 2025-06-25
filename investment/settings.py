607900d018132e2f8655b3ba41bc7d86ea01d24e
marvidik <ebubeidika@gmail.com>
2025-06-25 20:50:11 +0100

    this is a commit by amanda

--- investment/settings.py
+++ investment/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-!(658t-9#l$+hqrvlq8i5kgwqgvung+8vl!wy*+qx-4n)8fi7c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
